3e52fd95db5fd98f55d6249edb86be813def5e8e
ysy56 <ysy561356@naver.com>
2024-02-28 15:51:47 +0900

    feat: 블로그 글 조회

--- src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogApiController.java
@@ -8,10 +8,7 @@ import org.example.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,5 +38,15 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    // URL 경로에서 값 추출
+    // URL에서 {id}에 해당하는 값이 id로 들어옴
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
--- src/test/java/org/example/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/org/example/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -99,4 +99,27 @@ class BlogApiControllerTest {
                 .andExpect(jsonPath("$[0].content").value(content))
                 .andExpect(jsonPath("$[0].title").value(title));
     }
+
+    @DisplayName("findArticle: 블로그 글 조회에 성공한다.")
+    @Test
+    public void findArticle() throws Exception {
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(get(url, savedArticle.getId()));
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.title").value(title));
+    }
 }
