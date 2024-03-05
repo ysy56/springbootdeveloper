65db1bd12d9fcb3e7a4d72ad9cebea764538d662
ysy56 <ysy561356@naver.com>
2024-03-05 23:25:12 +0900

    test: 주석 추가

--- src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing // created_at, updated_at 자동 업데이트
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
+    // test
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
